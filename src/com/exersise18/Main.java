eb8a782b78a357f1241fd4bf89458b7fea71fd4b
AmeliaAnn <53219102+AmeliaAnn@users.noreply.github.com>
2019-12-17 15:16:15 +0300

    Update Main.java

--- src/com/exersise18/Main.java
+++ src/com/exersise18/Main.java
@@ -4,41 +4,42 @@ import java.io.*;
 import java.io.InputStreamReader;
 public class Main {
 
-    public CountStr() {
+     public static int CountStr() {
 
         final File file = new File("test.txt");
-
-        {
-            LineNumberReader lnr = null;
+        LineNumberReader lnr = null;
+        try {
+            lnr = new LineNumberReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int linesCount = 0;
+        while (true) {
             try {
-                lnr = new LineNumberReader(new FileReader(file));
-            } catch (FileNotFoundException e) {
+                if (null == lnr.readLine())
+                    break;
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            int linesCount;
-            linesCount = 0;
-            while (true) {
-                try {
-                    if (null != lnr.readLine()) break;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                linesCount += 1;
-            }
-            System.out.println("Количество строк в файле: " + linesCount);
+            linesCount += 1;
         }
+        System.out.println("Количество строк в файле: " + linesCount);
+        return linesCount;
     }
 
-    public static void main(String[] args, String linesCount)
-    {
+    public static void main(String[] args) {
 
+        int countStr = CountStr();
+        if(countStr == 0) {
+            System.out.println("Файл был пустой");
+            return;
+        }
+        
         System.out.println("Вывод содержимого файла: ");
         String s;
-        try (BufferedReader br =
-                new BufferedReader (new FileReader ("test.txt")))
-        {
+        try (BufferedReader br = new BufferedReader(new FileReader("test.txt"))) {
             while ((s = br.readLine()) != null) {
-                System.out.println (s);
+                System.out.println(s);
             }
         } catch (IOException exc) {
             System.out.println("Ошибка вывода: " + exc);
@@ -47,22 +48,26 @@ public class Main {
 
         String str;
         BufferedReader br =
-                new BufferedReader (
+                new BufferedReader(
                         new InputStreamReader(System.in));
         System.out.println("Запись в файл закончится, когда строк будет столько же, сколько было в файле: ");
 
         try (FileWriter fw = new FileWriter("test.txt")) {
-
             do {
+                countStr--;
                 System.out.print(": ");
-                str = br.readLine();
-                if (int i = str.compareTo(linesCount);
-                str = str + "\r\n";
+                if(countStr == 0) {
+                    str = br.readLine();
+                } else {
+                    str = br.readLine() + "\r\n";
+                }
                 fw.write(str);
-            } while (str.compareTo(linesCount) != 0);
-        } catch(IOException exc) {
+            } while (countStr != 0);
+        } catch (IOException exc) {
             System.out.println("Ошибка ввода-вывода: " + exc);
 
+        }
+    }
         }
     }
 }
